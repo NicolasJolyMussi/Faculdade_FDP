129a6e5d387e73a2f727f30981928110600bdea5
Nícolas Mussi <nicolas.jm.contato@gmail.com>
2025-08-27 15:44:16 -0300

    Exercícios C#.cs

---
+++ Exercícios C#.cs
@@ -0,0 +1,46 @@
+Console.WriteLine("Exercício 1");
+Console.WriteLine("Contagem de 1 até 1.000:");
+
+for (int i = 1; i <= 1000; i++)
+{
+    Console.WriteLine("Número:" + i);
+}
+Console.WriteLine("Contagem finalizada.");
+Console.WriteLine("+----------------------------------------------------------+");
+
+Console.WriteLine("+----------------------------------------------------------+");
+
+Console.WriteLine("+----------------------------------------------------------+");
+
+
+Console.WriteLine("Exercício 2");
+Console.WriteLine("Contagem de 1 até 1.000:");
+int contador = 1;
+while (contador <= 1000) {
+
+    Console.WriteLine("Número" + contador);
+    contador++;
+}
+Console.WriteLine("Contagem finalizada");
+Console.WriteLine("+----------------------------------------------------------+");
+
+Console.WriteLine("+----------------------------------------------------------+");
+
+Console.WriteLine("+----------------------------------------------------------+");
+
+
+Console.WriteLine("Exercício 3");
+Console.WriteLine("Contagem de 1 até 1.000");
+int contadorr = 1;
+do
+{
+    Console.WriteLine("Número:" + contadorr);
+    contadorr++;
+}
+while (contadorr <= 1000);
+Console.WriteLine("Contagem finallizada");
+Console.WriteLine("+----------------------------------------------------------+");
+
+Console.WriteLine("+----------------------------------------------------------+");
+
+Console.WriteLine("+----------------------------------------------------------+");
